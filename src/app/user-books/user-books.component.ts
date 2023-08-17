92abecb8b954d971fd2209f89ddeacffe83034d2
Luca Colombini <luca.colombini81@gmail.com>
2023-08-17 09:57:23 +0200

    Added book service

--- src/app/user-books/user-books.component.ts
+++ src/app/user-books/user-books.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
 import { Observable } from 'rxjs';
+import { BookService } from '../services/book.service';
 
 @Component({
   selector: 'app-user-books',
@@ -9,12 +10,16 @@ import { Observable } from 'rxjs';
 })
 export class UserBooksComponent implements OnInit {
 
+    bookList:any[] = []
     constructor(
         private authenticationService: AuthenticationService,
+        private bookService:BookService
     ) { }
 
     ngOnInit(): void { 
-        console.log("PROVA DI CONNESSIONE ONINIT")
+        const userId = this.authenticationService.getUserId()
+        this.bookList = this.bookService.bookList(userId);
+        console.log("LISTA: " + JSON.stringify(this.bookList))
     }
 
     logout(): void {
