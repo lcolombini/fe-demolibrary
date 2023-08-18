2ffdf8c87cd009ace5611115c46fb0604485fcb1
Luca Colombini <luca.colombini81@gmail.com>
2023-08-18 09:57:17 +0200

    Added action to updated field of selected book

--- src/app/book-detail/book-detail.component.ts
+++ src/app/book-detail/book-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
 import { BookService } from '../services/book.service';
 import { Book } from '../interfaces/book';
+import { BookClient } from '../clients/book.client';
 
 @Component({
   selector: 'app-book-detail',
@@ -15,10 +16,39 @@ export class BookDetailComponent implements OnInit{
     constructor(
         private authenticationService: AuthenticationService,
         private bookService: BookService,
+        private bookClient: BookClient
     ){}
     ngOnInit(): void {
         this.book = this.bookService.getBook()
-        console.log("LIBRO DOPO MODIFICA: " + JSON.stringify(this.book))
+    }
+
+    public actionRead(book:any)
+    {
+        console.log("Read action called!")
+        const bookToRead = book as Book
+        const userId = this.authenticationService.getUserId()
+
+        try {
+            this.bookClient.read(bookToRead.id, Number(userId)).subscribe((res) => {
+                console.log(res)
+            });
+        } catch (error) {
+            throw error
+        }
+    }
+
+    public actionRemove(book:any)
+    {
+        console.log("Remove action called!")
+        const bookToRead = book as Book
+        const userId = this.authenticationService.getUserId()
+        try {
+            this.bookClient.remove(bookToRead.id, Number(userId)).subscribe((res) => {
+                console.log(res)
+            });
+        } catch (error) {
+            throw error
+        }
     }
 
 }
